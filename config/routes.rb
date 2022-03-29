a93c069777faec10a573e7d16fc039288f5f2b5b
Duangrudee Methakullachat <blufairie888@gmail.com>
2022-03-29 23:39:02 +0900

    links and navbar working

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get 'portfolio/index', to: 'portfolio#index', as: 'portfolio'
+  get 'hobby/index', to: 'hobby#index', as: 'hobby'
+  get 'contact/index', to: 'contact#index', as: 'contact'
   root to: 'pages#welcome'
 end
