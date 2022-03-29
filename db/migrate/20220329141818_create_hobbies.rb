8397fdeb863b91a4187182c2e4e83dbfbc59986a
Duangrudee Methakullachat <blufairie888@gmail.com>
2022-03-29 23:20:41 +0900

    added contact and hobby page

---
+++ db/migrate/20220329141818_create_hobbies.rb
@@ -0,0 +1,8 @@
+class CreateHobbies < ActiveRecord::Migration[6.1]
+  def change
+    create_table :hobbies do |t|
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_27_132205) do
+ActiveRecord::Schema.define(version: 2022_03_29_141839) do
+
+  create_table "contacts", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "hobbies", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "portfolios", force: :cascade do |t|
     t.string "name"
