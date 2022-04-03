bfbd72d50d4f1db93108e46ac5de55046b2f60c0
Duangrudee Methakullachat <blufairie888@gmail.com>
2022-04-03 17:35:37 +0900

    changed db to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2022_03_30_052752) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "contacts", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
